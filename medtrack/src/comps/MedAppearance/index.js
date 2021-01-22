ed62dfc19e05aa0e0e6ef97d6f5da4f3dfccd3b9
cpark3918 <cindypark31@gmail.com>
2021-01-22 15:54:14 -0800

    added med appearance

---
+++ medtrack/src/comps/MedAppearance/index.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+
+`;
+
+
+const MedAppearance = ({}) => {
+    return <Container >
+
+    </Container> 
+};
+
+MedAppearance.defaultProps = {
+
+};
+
+export default MedAppearance;
