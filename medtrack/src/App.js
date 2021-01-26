b30480c227ff9338809dbd5ecadd2d5f91a35886
cpark3918 <cindypark31@gmail.com>
2021-01-26 01:09:41 -0800

    basic react-router-dom

--- medtrack/src/App.js
+++ medtrack/src/App.js
@@ -1,10 +1,16 @@
 import './App.scss';
-// import MedAppearance from 'comps/MedAppearance';
+import MedAppearance from 'comps/MedAppearance';
+import Main from "comps/Main";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       {/* <MedAppearance /> */}
+      <Main />
+      <Link to ="/add-med">
+          <button>start</button>
+      </Link>
     </div>
   );
 }
