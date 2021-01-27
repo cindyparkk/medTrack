2a63509f26c5da8ca9acf8821791ae5639d9b328
kliang96 <570789@gmail.com>
2021-01-27 13:44:59 -0800

    changes

--- medtrack/src/comps/Info/index.js
+++ medtrack/src/comps/Info/index.js
@@ -3,27 +3,72 @@ import styled from "styled-components";
 
 const Container = styled.div`
   max-width: 100vw;
-  padding: 10px;
-  background-color: #e5e5e5;
   display: inline-flex;
   flex-direction: column;
+`;
+const ContainerInner = styled.div`
+  max-width: 100vw;
+  display: inline-flex;
+  flex-direction: row;
   justify-content: center;
   align-items: center;
-  h1 {
-    margin-bottom: 15px;
-    margin-left: 15px;
-  }
 `;
 
+const IconLeft = styled.img`
+  max-height: 35px;
+  width: auto;
+  padding: 0px 15px;
+`;
+const IconRight = styled.img`
+  max-height: 45px;
+  width: auto;
+  padding-left: 15px;
+  margin: 0px;
+`;
 
-const Info = ({}) => {
-    return (
-      <Container>
+const InnerContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+`;
 
-      </Container>
-    );
-  };
-  
-  Info.defaultProps = {};
-  export default Info;
-  
+const Title = styled.h1`
+  margin: 0px 10px;
+  padding: 0px;
+`;
+const Subtext = styled.p`
+  margin: 5px 11px;
+  padding: 0px;
+`;
+const Divide = styled.hr`
+  display: block;
+  height: 1px;
+  border: 0;
+  border-top: 2px solid #ccc;
+  margin: 1px 0;
+  padding: 0;
+`;
+
+const Info = ({ leftimgurl, title, subtext, rightimgurl }) => {
+  return (
+    <Container>
+      <ContainerInner>
+        <IconLeft src={leftimgurl} />
+        <InnerContainer>
+          <Title>{title}</Title>
+          <Subtext>{subtext}</Subtext>
+        </InnerContainer>
+        <IconRight src={rightimgurl} />
+      </ContainerInner>
+      <Divide />
+    </Container>
+  );
+};
+
+Info.defaultProps = {
+  leftimgurl: "/stopwatch.png",
+  rightimgurl: "/right.png",
+  title: "Title Content",
+  subtext: "08:00 AM/15mg",
+};
+export default Info;
